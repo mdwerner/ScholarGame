98fb12458793225f174ff237cef918a9f6ef0364
mdwerner <werner.michael.d@gmail.com>
2018-09-24 15:43:51 -0500

    Fresh Project
    A new empty project that compiles.

---
+++ desktop/src/com/scholar/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.scholar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.scholar.ScholarGame;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ScholarGame(), config);
+	}
+}
